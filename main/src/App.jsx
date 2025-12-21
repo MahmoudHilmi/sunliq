efcf17f1afb43b008fb84a3d42fda6e5fd7cf71f
Mahmoud <m.hilmiharb1@gmail.com>
2025-12-21 18:42:46 +0200

    Initialize Project Base

---
+++ main/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className='bg-blue-500 w-screen h-screen'>
+
+      <h1 className='font-black text-5xl'>Hello Sunliq</h1>
+ 
+    </div>
+  )
+}
+
+export default App
